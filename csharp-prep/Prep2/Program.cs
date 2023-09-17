09030a6a43a160d7b953146ca7405b6a192a6133
MAL3F0R <2isaachammes@gmail.com>
2023-09-16 22:29:55 -0600

    Second prep assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage for your class? ");
+        string value = Console.ReadLine();
+        int percent = int.Parse(value);
+        string grade = "";
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+        else if (percent >= 70)
+        {
+            grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+        Console.WriteLine($"Your current grade is {grade}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Your grade is at a passing level.");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is not at a passing level.");
+        }
     }
 }
