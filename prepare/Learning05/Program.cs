b26e5c5073a2c0dd76d0be7d705f22b26000a90e
MAL3F0R <2isaachammes@gmail.com>
2023-10-11 19:59:07 -0600

    prove project W5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Fraction t1 = new Fraction();
+        Console.WriteLine(t1.GetFractionString());
+        Console.WriteLine(t1.GetDecimalValue());
+        
+        Fraction t2 = new Fraction(5);
+        Console.WriteLine(t2.GetFractionString());
+        Console.WriteLine(t2.GetDecimalValue());
+
+        Fraction t3 = new Fraction(3, 4);
+        Console.WriteLine(t3.GetFractionString());
+        Console.WriteLine(t3.GetDecimalValue());
+
+        Fraction t4 = new Fraction(1, 3);
+        Console.WriteLine(t4.GetFractionString());
+        Console.WriteLine(t4.GetDecimalValue());
     }
 }
